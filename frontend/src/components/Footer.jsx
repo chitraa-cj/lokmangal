bfbd348869d36e18857308769702a0ef808b7696
Sooraj Gupta <soorajgupta00@gmail.com>
2025-03-02 16:13:49 +0530

    update

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,13 +1,12 @@
 const Footer = () => {
   const newsCategories = [
     {
-      title: "News From Indian States",
+      title: "States News",
       links: [
         "Madhya Pradesh News",
         "Uttar Pradesh News",
         "Rajasthan News",
         "Haryana News",
-        "Bihar News",
       ],
     },
     {
@@ -28,15 +27,20 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <div className="flex w-full flex-col items-center justify-center bg-gray-800 px-4 py-8 text-white">
-      <div className="grid grid-cols-1 gap-12 md:grid-cols-3">
+    <div className="flex w-full flex-col items-start justify-center bg-gray-800 px-4 py-8 text-white lg:items-center">
+      <div className="ml-7 flex w-full items-center justify-evenly md:ml-0">
         {newsCategories.map((category, index) => (
-          <div key={`category-${index}`} className="md:col-span-1">
-            <h3 className="mb-4 font-semibold">{category.title}</h3>
+          <div key={`category-${index}`} className="md:col-span-1 md:ml-10">
+            <h3 className="mb-3 mt-4 text-lg font-semibold">
+              {category.title}
+            </h3>
             <ul className="space-y-2">
               {category.links.map((link, i) => (
                 <li key={`link-${index}-${i}`}>
-                  <a href="#" className="text-gray-400 hover:text-gray-100">
+                  <a
+                    href="#"
+                    className="text-center text-gray-400 hover:text-gray-100"
+                  >
                     {link}
                   </a>
                 </li>
@@ -46,13 +50,16 @@ const Footer = () => {
         ))}
       </div>
 
-      <footer className="mt-12 border-t border-gray-200 pt-8">
-        <div className="grid grid-cols-1 gap-12 md:grid-cols-3">
+      <footer className="mt-12 w-full border-t border-gray-200 px-2 pt-8 lg:px-28">
+        <div className="flex flex-col items-center justify-between lg:flex-row">
           <div>
-            <h4 className="mb-4 font-semibold">Quick Links</h4>
+            {/* <h4 className="font-semibold lg:mb-4">Quick Links</h4> */}
             <div className="flex flex-wrap gap-2">
               {["About Us", "Careers", "Contact Us"].map((link, index) => (
-                <span key={`quicklink-${index}`} className="flex items-center">
+                <span
+                  key={`quickLink-${index}`}
+                  className="my-2 flex items-center lg:my-0"
+                >
                   <a
                     href="#"
                     className="text-sm text-gray-400 hover:text-gray-100"
@@ -62,19 +69,6 @@ const Footer = () => {
                   {index < 2 && <span className="mx-1 text-gray-300">|</span>}
                 </span>
               ))}
-            </div>
-          </div>
-          <div>
-            <h4 className="mb-4 font-semibold">Subscribe</h4>
-            <div className="flex">
-              <input
-                type="email"
-                placeholder="Enter your email"
-                className="flex-1 rounded-l border border-gray-300 px-4 py-2"
-              />
-              <button className="rounded-r bg-red-600 px-6 py-2 text-white">
-                Subscribe
-              </button>
             </div>
           </div>
           <div className="text-right">
