83c2237eecc08904ae32911e2c3d3d03caa9e766
Sooraj Gupta <soorajgupta00@gmail.com>
2025-03-16 10:56:46 +0530

    update

--- frontend/src/components/BreakingNews.jsx
+++ frontend/src/components/BreakingNews.jsx
@@ -10,11 +10,10 @@ const BreakingNews = ({ breakingNews }) => {
   console.log(breakingNews);
 
   return (
-    // <div className="mb-4 flex w-full max-w-2xl items-center justify-between space-x-1 rounded-md bg-red-600 p-2 text-xs text-white shadow-md sm:mb-4 sm:space-x-2 sm:text-sm lg:rounded-full lg:px-4">
-    <div className="mb-4 flex w-full max-w-2xl items-center justify-between space-x-1 rounded-lg bg-red-600 p-2 text-xs text-white shadow-md sm:mb-4 sm:space-x-2 sm:text-sm lg:px-4">
-      <span className="font-bold">BREAKING NEWS</span>
+    <div className="mb-4 flex w-full max-w-3xl items-center justify-between space-x-1 rounded-lg bg-red-600 px-2 py-[6px] text-xs text-white shadow-md sm:mb-4 sm:space-x-2 sm:text-sm lg:rounded-full lg:px-4">
+      <span className="text-xl font-bold">BREAKING NEWS</span>
       <span
-        className="flex-1 cursor-pointer items-center justify-center border-l pl-2 text-lg sm:pl-4 lg:truncate"
+        className="line-clamp-2 flex-1 cursor-pointer items-center justify-center border-l pl-2 text-lg font-semibold sm:pl-4"
         onClick={() => onClickNavigate(breakingNews[0])}
       >
         {/* JK: कठुआ जिले के बिलावर इलाके में मिले 3 लापता नागरिकों के शव - सूत्र */}
