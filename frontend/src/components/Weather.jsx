5161d985e2688b484a2f2ec8136492da2e60525d
Sooraj Gupta <soorajgupta00@gmail.com>
2025-03-20 14:12:43 +0530

    update

--- frontend/src/components/Weather.jsx
+++ frontend/src/components/Weather.jsx
@@ -86,7 +86,8 @@ const Weather = () => {
     const statuses = {
       1: { text: "Good", color: "text-green-600" },
       2: { text: "Moderate", color: "text-yellow-500" },
-      3: { text: "Unhealthy Sensitive groups", color: "text-orange-500" },
+      // 3: { text: "Unhealthy for Sensitive groups", color: "text-orange-500" },
+      3: { text: "Risk for Sensitive", color: "text-orange-500" },
       4: { text: "Unhealthy", color: "text-red-500" },
       5: { text: "Very Unhealthy", color: "text-purple-600" },
       6: { text: "Hazardous", color: "text-red-800" },
